7c6b9f957e5d420e04d88433ac86219460ba0fcb
ThomasGhignon <ghignon.thomas@gmail.com>
2020-05-28 14:52:56 +0200

    up

--- resources/views/pages/homeIndividual.blade.php
+++ resources/views/pages/homeIndividual.blade.php
@@ -10,7 +10,11 @@
 
 @section('content')
 
-
+    <svg width="0" height="0" class="hidden">
+        <symbol viewBox="0 0 1280 621" fill="none" xmlns="http://www.w3.org/2000/svg" id="Subtract">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M1280 102.881L0 0v621h1280V102.881z" fill="#fff"></path>
+        </symbol>
+    </svg>
 
 @endsection
 
