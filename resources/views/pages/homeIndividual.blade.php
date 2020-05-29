a413ebc7447382e286d58defaaeb06b6260d8eab
ThomasGhignon <ghignon.thomas@gmail.com>
2020-05-29 12:18:06 +0200

    up

--- resources/views/pages/homeIndividual.blade.php
+++ resources/views/pages/homeIndividual.blade.php
@@ -57,7 +57,7 @@
     <section class="topSport">
         {{--<div class="splitterBanner"> </div>--}}
         <div class="wrap">
-            <h1>Le top des <span>sport</span> les plus <span>consultés</span></h1>
+            <h1>Le top des <span>sports</span> les plus <span>consultés</span></h1>
             <div>
                 <div><img src="" alt=""></div>
                 <div><img src="" alt=""></div>
@@ -77,6 +77,8 @@
         <div class="wrap">
             <h1>WeGuide<span>News</span></h1>
             <div>
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="quote-left" class="svg-inline--fa fa-quote-left fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"></path></svg>
+                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="quote-left" class="svg-inline--fa fa-quote-left fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"></path></svg>
                 <div>
                     <div><img src="{{asset('/img/dane-wetton-t1NEMSm1rgI-unsplash.jpg')}}" alt="exemple article"></div>
                     <h2>Istum ipsum sic quem cave.
