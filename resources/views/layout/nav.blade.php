c48e741f12715fb2f06470177258a3ec93a5ac38
ThomasGhignon <ghignon.thomas@gmail.com>
2020-06-11 16:03:31 +0200

    up

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -11,13 +11,17 @@
 			</a>
 			<a href="#">Poster une annonce</a>
 			<a href="/favoris">Favoris</a>
-			<a href="#" class="toggleModalProfil js-toggleConnectionContainer">Connexion</a>
-			@include('components.buttonLink', ['newId' => 'js-registrationBtn','link' => '/register','text' => 'Inscription'])
-			<span class="toggleModalProfil hidden">
-				<div class="img"></div>
-				<p>Megan</p>
-				<i class="fa fa-chevron-down arrow"></i>
-			</span>
+            @if(auth()->check() === false)
+                <a href="#" class="toggleModalProfil js-toggleConnectionContainer">Connexion</a>
+                @include('components.buttonLink', ['newId' => 'js-registrationBtn','link' => '/register','text' => 'Inscription'])
+            @endif
+            @if(auth()->check() === true)
+                <span class="toggleModalProfil hidden">
+                    <div class="img"></div>
+                    <p>Megan</p>
+                    <i class="fa fa-chevron-down arrow"></i>
+                </span>
+            @endif
 		</div>
 	</div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('pages/home');
 });
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/particulier', function () {
     return view('pages/homeIndividual');
 });
@@ -104,8 +108,3 @@ Route::get('addAdvert', 'AdvertController@addAdvert');
 // Create Advertisement
 Route::get('/deposer-une-annonce', 'Create_AdvertisementController@create');
 Route::post('/deposer-une-annonce', 'Create_AdvertisementController@store');
-
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
