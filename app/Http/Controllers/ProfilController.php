b2d287190d1a3d3a0173724c86c5289ba7c9b22b
ThomasGhignon <ghignon.thomas@gmail.com>
2020-06-15 17:52:04 +0200

    up profil

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class ProfilController extends Controller
 {
     public function index(){
         if (Auth::check()){
-            $user = Auth::user();
-            return view('pages/profil', ['user'=>$user]);
+            $Carbon=  Carbon::now();
+            $dateOfBirth = Auth::user()->birth;
+            $years = Carbon::createFromDate($dateOfBirth)->age;
+            return view('pages/profil', ['years'=>$years]);
         }
     }
 }
