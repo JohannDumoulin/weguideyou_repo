5ec187a686f5f0e16159e4c484388a94c715d94d
ThomasGhignon <ghignon.thomas@gmail.com>
2020-06-16 17:26:05 +0200

    up reviews table, model

---
+++ database/migrations/2020_06_16_143157_create_reviews_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('id_sender');
+            $table->foreign('id_sender')->references('id')->on('users');
+            $table->unsignedInteger('id_receiver');
+            $table->foreign('id_receiver')->references('id')->on('users');
+            $table->text('description');
+            $table->integer('rating');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
