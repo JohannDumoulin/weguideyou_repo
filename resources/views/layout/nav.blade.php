960939c523bee30d7d6997a8d5905772f3679602
ThomasGhignon <ghignon.thomas@gmail.com>
2020-06-16 10:15:30 +0200

    Update nav.blade.php

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -9,25 +9,23 @@
 				<i class="fa fa-globe"></i>
 				<i class="fa fa-chevron-down arrow"></i>
 			</a>
-            @if(Auth::check())
-			<a href="{{ route('create_ad') }}">Poster une annonce</a>
-			<a href="/favoris">Favoris</a>
-            @endif
-
-            @if(!Auth::check())
+            @auth
+                <a href="{{ route('create_ad') }}">Poster une annonce</a>
+                <a href="/favoris">Favoris</a>
+            @endauth
+            @guest
                 <a href="#" class="js-toggleConnectionContainer">Connexion</a>
                 @include('components.buttonLink', ['newId' => 'js-registrationBtn','link' => '/register','text' => 'Inscription'])
-            @endif
-
-            @if(Auth::check())
+            @endguest
+            @auth
                 <div class="js-toggleModalProfil">
                     <div>
-                        <img src="{{asset('img/megan1.jpg')}}" alt="Image de profil">
+                        <img src="{{ Auth::user()->pic ?? asset('img/user-circle-solid-white.svg')}}" alt="Image de profil">
                     </div>
-                    <span>{{ $user['name'] ?? 'undefined' }}</span>
+                    <span>{{ Auth::user()->name ?? 'undefined' }}</span>
                     <i class="fa fa-chevron-down arrow"></i>
                 </div>
-            @endif
+            @endauth
 		</div>
 	</div>
 </nav>
