84ae9357156e1f8d513dc416e33ac8ef3c49dbfe
gwendallefort <65173197+gwendallefort@users.noreply.github.com>
2020-06-19 11:23:22 +0200

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ $factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'password' => Hash::make("123456"),
         'surname' => "surname",
         'gender' => "H",
         'birth' => date_create("1990-05-15"),
